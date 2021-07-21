c48c48b02eca79789dfdc9da257d2b709e387c7d
Reywaltz <george1147@yandex.ru>
2021-07-21 23:30:08 +0300

    Added logger interface

---
+++ task-api/pkg/logger/ABClog.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Logger(ABC):
+    """Интерфейс создания логгера"""
+    @abstractmethod
+    def info(self, msg: Any):
+        pass
+
+    @abstractmethod
+    def warning(self, msg: Any):
+        pass
+
+    @abstractmethod
+    def error(self, msg: Any):
+        pass
+
+    @abstractmethod
+    def fatal(self, msg: Any):
+        pass
